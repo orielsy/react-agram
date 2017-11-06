4b66f2a288f0a10c7162e0460611a3e74ae20d0c
orielsy <orielsydiaz@gmail.com>
2017-11-06 08:55:28 -0500

    update

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
+  
     return (
       <div className="photo-grid">
-      I'm the photo grid
+        {this.props.posts.map(
+          (post, i) => {
+            return <Photo {...this.props} key={i} i={i} post={post}/>
+          })
+        }        
       </div>
     )
+    
   }
 });
 
